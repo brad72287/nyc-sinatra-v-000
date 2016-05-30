1bf4d4ab6a5296011e52eb72859ae4cd0c9d4479
breiss <brad.p.reiss@gmail.com>
2016-05-30 09:51:03 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,56 @@
 class FiguresController < Sinatra::Base
+    set :views, Proc.new { File.join(root, "../views/") }
+
+    get "/figures/new" do
+        @titles = Title.all
+        @landmarks = Landmark.all
+        erb :'figures/new'
+    end
+    
+    get "/figures/:id/edit" do
+        @figure = Figure.find(params[:id])
+        @titles = Title.all
+        @landmarks = Landmark.all
+        erb :'figures/edit'
+    end
+    
+    patch "/figures" do
+        puts params
+        @figure = Figure.find(params[:figure][:id])
+        @figure.name = params[:figure][:name]
+        params[:figure][:landmark_ids].each {|x| @figure.landmarks << Landmark.find(x)} unless params[:figure][:landmark_ids].nil?
+        params[:figure][:title_ids].each {|x| @figure.titles << Title.find(x)} unless params[:figure][:title_ids].nil?
+        @figure.titles << Title.create(name: params[:new_title]) unless params[:new_title].nil?
+        @figure.landmarks << Landmark.create(name: params[:new_landmark]) unless params[:new_landmark].nil?
+        @figure.save
+        redirect to "/figures/#{@figure.id}"
+        #"How about a nice patch request?"
+    end
+   
+    get "/figures/:id" do
+        @item = Figure.find(params[:id])
+        erb :"figures/show"
+    end
+    
+
+    
+    get "/figures" do
+        @list = Figure.all
+        erb :'figures/index'
+    end
+    
+    post "/figures" do
+        puts params
+        @figure = Figure.create(name: params[:figure][:name])
+        params[:figure][:landmark_ids].each {|x| @figure.landmarks << Landmark.find(x)} unless params[:figure][:landmark_ids].nil?
+        params[:figure][:title_ids].each {|x| @figure.titles << Title.find(x)} unless params[:figure][:title_ids].nil?
+        @figure.titles << Title.create(name: params[:new_title]) unless params[:new_title].nil?
+        @figure.landmarks << Landmark.create(name: params[:new_landmark]) unless params[:new_landmark].nil?
+        @figure.save
+        params.to_s
+    end
+
 
 end
+
+
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,40 @@
 class LandmarksController < Sinatra::Base
+      set :views, Proc.new { File.join(root, "../views/") }
+      
+    get "/landmarks/new" do
+        erb :"landmarks/new"
+    end
+    
+    get "/landmarks/:id/edit" do
+        @landmark = Landmark.find(params[:id])
+        erb :"landmarks/edit"
+    end
+    
+    get "/landmarks" do
+        @list = Landmark.all
+        erb :'landmarks/index'
+    end
+    
+    get "/landmarks/:id" do
+        @landmark = Landmark.find(params[:id])
+        erb :"landmarks/show"
+    end
+    
+    post "/landmarks" do
+        puts params
+        @landmark = Landmark.create(name: params[:landmark_name])
+        @landmark.year_completed = params[:landmark_year_completed]
+        @landmark.save
+        params.to_s
+    end
+    
+    patch "/landmarks" do
+        puts params
+        @landmark = Landmark.find(params[:landmark][:id])
+        @landmark.name = params[:landmark][:name]
+        @landmark.year_completed = params[:landmark][:year_completed]
+        @landmark.save
+        redirect to "/landmarks/#{@landmark.id}"
+    end
 
 end
